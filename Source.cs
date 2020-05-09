f1871b386faed8faa26f3700e0e844f4be771567
Mathmatician <38021845+Mathmatician@users.noreply.github.com>
2020-05-08 23:38:03 -0600

    Update Source.cs

--- Source.cs
+++ Source.cs
@@ -7,12 +7,12 @@ namespace Company_Application
         static void Main(string[] args)
         {
             // Employee emp = new Employee("FirstName", "LastName", "Address", PhoneNumber, EmployeeNumber, Salary);
-            Employee Jerry = new Employee("Albert", "Einstein", "3450 Thunderbluff, New York, NY, 58156", 123456789, 117, 1000000000);
-            Jerry.PrintInfo();
+            Employee Albert = new Employee("Albert", "Einstein", "3450 Thunderbluff, New York, NY, 58156", 123456789, 117, 1000000000);
+            Albert.PrintInfo();
 
             // Client clnt = new Client("FirstName", "LastName", "Address", PhoneNumber, AccountNumber);
-            Client Jerome = new Client("Paul", "Dirac", "5236N Iron Forge Way, Salt Lake City, Utah, 85214", 6543219876, 343);
-            Jerome.PrintInfo();
+            Client Paul = new Client("Paul", "Dirac", "5236N Iron Forge Way, Salt Lake City, Utah, 85214", 6543219876, 343);
+            Paul.PrintInfo();
             
             Console.ReadKey();
         }
