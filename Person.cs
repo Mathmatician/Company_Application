af4de242a646a1d27cfbc0e8f2a7313a23089443
Mathmatician <38021845+Mathmatician@users.noreply.github.com>
2020-05-08 23:13:41 -0600

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Person_Space
+{
+    class Person
+    {
+        // Initialization Variables //
+        protected string firstName = "";
+        protected string lastName = "";
+        protected string address = "";
+        protected long phoneNumber = 0;
+
+
+        /*----------------
+         | Set Functions |
+         ---------------*/
+        private void setFirstName(string fnm)
+        {
+            firstName = fnm;
+        }
+
+        private void setLastName(string lnm)
+        {
+            lastName = lnm;
+        }
+
+        private void setAddress(string addr)
+        {
+            address = addr;
+        }
+
+        private void setPhoneNumber(long pnum)
+        {
+            phoneNumber = pnum;
+        }
+
+
+
+        /*-------------------
+         | Return functions |
+         ------------------*/
+        public string getName()
+        {
+            return firstName + " " + lastName;
+        }
+
+        public string getAddress()
+        {
+            return address;
+        }
+
+        public long getPhoneNumber()
+        {
+            return phoneNumber;
+        }
+    }
+}
